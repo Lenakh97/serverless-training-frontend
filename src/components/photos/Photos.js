d9a25afb3a65abb8789eb6b733d6885f9d2f5762
lenakh97 <lenaharaldseid@gmail.com>
2024-01-04 15:08:42 +0100

    feat: use github secrets in stead of hard coding in config.json

--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -1,65 +1,68 @@
-import React, { Component, Fragment } from 'react';
-import { API, Storage, Auth } from 'aws-amplify';
-import ImageGallery from 'react-image-gallery';
-import config from '../../config';
+import React, { Component, Fragment } from "react";
+import { API, Storage, Auth } from "aws-amplify";
+import ImageGallery from "react-image-gallery";
+import { fromEnv } from "@nordicsemiconductor/from-env";
 
-let images = [];  
+let images = [];
+const { thumbBucket } = fromEnv({ thumbBucket: "THUMBBUCKET" });
 
 async function getPresignedURLS(orig, thumb) {
-  let presignOriginal = await Storage.get(orig, { level: 'private' });
-  let presignThumb = await Storage.get(thumb, { level: 'private', bucket:  config.s3.thumbBucket});
-  let results = [ presignOriginal, presignThumb];
+  let presignOriginal = await Storage.get(orig, { level: "private" });
+  let presignThumb = await Storage.get(thumb, {
+    level: "private",
+    bucket: thumbBucket,
+  });
+  let results = [presignOriginal, presignThumb];
   return Promise.resolve(results);
 }
 
 async function getPhotoLabels(key) {
-  const apiName = 'imageAPI';
-  const path = '/images'; 
+  const apiName = "imageAPI";
+  const path = "/images";
 
-  const myInit = { // OPTIONAL
-    headers: { 
-      'Content-Type': 'application/json'
+  const myInit = {
+    // OPTIONAL
+    headers: {
+      "Content-Type": "application/json",
     },
     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
-    queryStringParameters: {  // OPTIONAL
-      action: 'getLabels',
-      key: key
+    queryStringParameters: {
+      // OPTIONAL
+      action: "getLabels",
+      key: key,
     },
   };
 
   let apiResponse = await API.get(apiName, path, myInit);
-  let results = [ apiResponse ];
+  let results = [apiResponse];
 
   return Promise.resolve(results);
 }
 
 export default class Photos extends Component {
-
   state = {
-    response: '',
-    filelist: '',
-    cognitoSub: '',
+    response: "",
+    filelist: "",
+    cognitoSub: "",
     loaded: false,
-    pr: ''
+    pr: "",
   };
 
   getId = () => {
-    Auth.currentUserInfo()
-      .then(response => {
-        // console.log(response.id)
-        this.setState({cognitoSub: response.id});
-      });
-  }
+    Auth.currentUserInfo().then((response) => {
+      // console.log(response.id)
+      this.setState({ cognitoSub: response.id });
+    });
+  };
 
   listImages = async () => {
-    
-    const result = await Storage.list('photos/', { level: 'private' });
-      
+    const result = await Storage.list("photos/", { level: "private" });
+
     let fileArray = Object.values(result);
     const cognitoID = this.state.cognitoSub;
 
-    const fileNames = fileArray.map(function (image) { 
-      return image.key.replace('photos/', ''); 
+    const fileNames = fileArray.map(function (image) {
+      return image.key.replace("photos/", "");
     });
 
     this.setState({ filelist: fileNames });
@@ -69,55 +72,51 @@ export default class Photos extends Component {
     fileNames.forEach(addImagesToList);
 
     function addImagesToList(filename) {
+      let orig = "photos/".concat(filename);
+      let fullName = "private/"
+        .concat(cognitoID)
+        .concat("/photos/")
+        .concat(filename);
+      getPresignedURLS(orig, orig).then((result) => {
+        let originalImageSigned = result[0];
+        let thumbImageSigned = result[1];
+        let currentImg = {
+          original: originalImageSigned,
+          thumbnail: thumbImageSigned,
+          description: "Testing Description",
+          isSelected: false,
+        };
+        images.push(currentImg);
 
-      let orig = 'photos/'.concat(filename);
-      let fullName = 'private/'.concat(cognitoID).concat('/photos/').concat(filename);
-      getPresignedURLS(orig, orig)
-        .then (result => { 
-          let originalImageSigned = result[0];
-          let thumbImageSigned = result[1];
-          let currentImg = {
-            original: originalImageSigned,
-            thumbnail: thumbImageSigned,
-            description: 'Testing Description',
-            isSelected: false,
-          };
-          images.push(currentImg);
+        getPhotoLabels(fullName).then((result) => {
+          let allLabels = result[0].data;
+          // console.log(allLabels)
+          if (allLabels) {
+            let labelsDetected = Object.values(allLabels);
 
-          getPhotoLabels(fullName)
-            .then (result => {  
+            const filterLabels = (cut, list) =>
+              list.filter((label) => !label.includes(cut));
 
-              let allLabels = result[0].data;
-              // console.log(allLabels)
-              if (allLabels) {
-                let labelsDetected = Object.values(allLabels);
-
-                const filterLabels = (cut, list) =>
-                  list.filter(label => !label.includes(cut));
-
-                let filtered = filterLabels('private', labelsDetected).join(' * ');
-                for (let i in images) {
-                  if (images[i].original.includes(filename)) {
-                    images[i].description = filtered;
-                    break; //Stop this loop, we found it!
-                  }
-                }
+            let filtered = filterLabels("private", labelsDetected).join(" * ");
+            for (let i in images) {
+              if (images[i].original.includes(filename)) {
+                images[i].description = filtered;
+                break; //Stop this loop, we found it!
               }
-            });
-        }); 
-
+            }
+          }
+        });
+      });
     }
-    this.setState({pr: true});
-
-
+    this.setState({ pr: true });
   };
 
   componentDidMount = async () => {
     this.getId();
     this.listImages();
-    this.setState({loaded: true});
-  }
-    
+    this.setState({ loaded: true });
+  };
+
   render() {
     return (
       <Fragment>
@@ -128,7 +127,9 @@ export default class Photos extends Component {
             <br />
             <div className="columns">
               <div className="column">
-                {this.state.pr ? <ImageGallery items={images} thumbnailPosition='left' />  : null }
+                {this.state.pr ? (
+                  <ImageGallery items={images} thumbnailPosition="left" />
+                ) : null}
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bulma/css/bulma.min.css';
-import 'react-image-gallery/styles/css/image-gallery.css';
-import './index.css';
-import App from './App';
-import Amplify, { Auth } from 'aws-amplify';
-import config from './config';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bulma/css/bulma.min.css";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./index.css";
+import App from "./App";
+import Amplify, { Auth } from "aws-amplify";
+import * as serviceWorker from "./serviceWorker";
+import { fromEnv } from "@nordicsemiconductor/from-env";
 
+const { region, userPoolId, userPoolWebClientId, identityPoolId, bucket } =
+  fromEnv({
+    region: "REGION",
+    userPoolId: "USER_POOL_ID",
+    userPoolWebClientId: "APP_CLIENT_ID",
+    identityPoolId: "IDENTITY_POOL_ID",
+    bucket: "BUCKET",
+    invokeUrl: "INVOKEURL",
+  });
 
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
+    region: region,
+    userPoolId: userPoolId,
+    userPoolWebClientId: userPoolWebClientId,
+    identityPoolId: identityPoolId,
   },
   Storage: {
-    region: config.cognito.REGION,
-    bucket: config.s3.bucket
+    region: region,
+    bucket: bucket,
   },
   API: {
     endpoints: [
       {
-        name: 'imageAPI',
-        endpoint: config.api.invokeUrl,
-        path: '/images',
-        custom_header: async () => { 
-          //   return { Authorization : 'token' } 
+        name: "imageAPI",
+        endpoint: invokeUrl,
+        path: "/images",
+        custom_header: async () => {
+          //   return { Authorization : 'token' }
           //   // Alternatively, with Cognito User Pools use this:
           //   // return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
           // return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
-          return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` };
-        }
-      }
-    ]
-  }
+          return {
+            Authorization: `Bearer ${(await Auth.currentSession())
+              .getIdToken()
+              .getJwtToken()}`,
+          };
+        },
+      },
+    ],
+  },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
